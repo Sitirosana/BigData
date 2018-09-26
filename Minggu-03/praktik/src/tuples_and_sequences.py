99a73181cc1c015f4f576282036f05dec9f030ac
Sitirosana <43399056+Sitirosana@users.noreply.github.com>
2018-09-26 21:25:31 +0700

    Create tuples_and_sequences.py

---
+++ Minggu-03/praktik/src/tuples_and_sequences.py
@@ -0,0 +1,15 @@
+>>> t = 12345, 54321, 'hello!'
+>>> t[0]
+12345
+>>> t
+(12345, 54321, 'hello!')
+>>> u = t, (1, 2, 3, 4, 5)
+>>> u
+((12345, 54321, 'hello!'), (1, 2, 3, 4, 5))
+>>> t[0] = 88888
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'tuple' object does not support item assignment
+>>> v = ([1, 2, 3], [3, 2, 1])
+>>> v
+([1, 2, 3], [3, 2, 1])

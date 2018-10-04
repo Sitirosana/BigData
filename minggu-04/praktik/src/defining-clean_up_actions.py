b4cc31ca3888d2ff27d106d9115cd40de7204314
Sitirosana <43399056+Sitirosana@users.noreply.github.com>
2018-10-04 15:51:32 +0700

    Create defining-clean_up_actions.py

---
+++ minggu-04/praktik/src/defining-clean_up_actions.py
@@ -0,0 +1,9 @@
+          >>> try:
+          ...     raise KeyboardInterrupt
+          ... finally:
+          ...     print('Goodbye, World!')
+          ...
+          Goodbye, World!
+          Traceback (most recent call last):
+            File "<stdin>", line 2, in <module>
+          KeyboardInterrupt

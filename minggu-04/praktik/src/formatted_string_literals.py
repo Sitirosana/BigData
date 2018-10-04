93d641a04f20f42ce52647a597df9b193967c95e
Sitirosana <43399056+Sitirosana@users.noreply.github.com>
2018-10-04 15:41:39 +0700

    Create formatted_string_literals.py

---
+++ minggu-04/praktik/src/formatted_string_literals.py
@@ -0,0 +1,10 @@
+            >>> import math
+            >>> print(f'The value of pi is approximately {math.pi:.3f}.')
+            The value of pi is approximately 3.142.
+            >>> table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+            >>> for name, phone in table.items():
+            ...     print(f'{name:10} ==> {phone:10d}')
+            ...
+            Sjoerd     ==>       4127
+            Jack       ==>       4098
+            Dcab       ==>       7678
